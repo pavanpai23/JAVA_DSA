a14ac8c2eb98ecefaad04a510f11a4fe15c219c0
pavanpai23 <pavanpai2006@gmail.com>
2025-09-28 22:41:34 +0530

    DSA

--- src/Array/trappingrainwater.java
+++ src/Array/trappingrainwater.java
@@ -1,14 +1,14 @@
-package Array;
-
-public class trappingrainwater {
-
-    public static int trappedrainwater(int height[]){
-        //left
-        int n= height.length;
-        int leftmax[]=new int[n];
-        
-    }
-    public static void main(String[] args) {
-
-    }
-}
+//package Array;
+//
+//public class trappingrainwater {
+//
+//    public static int trappedrainwater(int height[]){
+//        //left
+//        int n= height.length;
+//        int leftmax[]=new int[n];
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
---
+++ src/pratices/sum2.java
@@ -0,0 +1,12 @@
+package pratices;
+
+public class sum2 {
+    public static void main(String[] args) {
+            int num[]={2,7,11,15};
+            int sum=0;
+            for(int i=0;i<2;i++){
+                sum+=num[i];
+            }
+            System.out.println(sum + "");
+    }
+}
