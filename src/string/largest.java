0243f32dff6e876d50ce6be72a3f2ae1039fe5ca
pavanpai23 <pavanpai2006@gmail.com>
2025-10-09 23:15:25 +0530

    DSA

---
+++ src/string/largest.java
@@ -0,0 +1,15 @@
+package string;
+
+public class largest {
+    public static void main(String[] args) {
+        String fruits[]={"apple","banana","mango"};
+
+        String largest=fruits[0];
+        for (int i = 1; i < fruits.length; i++) {
+            if(largest.compareToIgnoreCase(fruits[i])<0){
+                largest=fruits[i];
+            }
+        }
+        System.out.println("largest is:"+largest);
+    }
+}
