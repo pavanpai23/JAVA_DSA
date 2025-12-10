441118b982e6a54aa028850b458f9ad18b91f37d
pavanpai23 <pavanpai2006@gmail.com>
2025-12-10 23:06:08 +0530

    JS

---
+++ src/DivideAndConqure/mergeSort.java
@@ -0,0 +1,13 @@
+package DivideAndConqure;
+
+public class mergeSort {
+    public static void print(int[]arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+"");
+            return;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
