42ca09f82c2a4fbfb301a15ed5e10f9ff4144a34
pavanpai23 <pavanpai2006@gmail.com>
2025-12-11 23:16:54 +0530

    DSA

--- src/DivideAndConqure/mergeSort.java
+++ src/DivideAndConqure/mergeSort.java
@@ -3,9 +3,18 @@ package DivideAndConqure;
 public class mergeSort {
     public static void print(int[]arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+"");
-            return;
+            System.out.println(arr[i] + "");
         }
+            System.out.println();
+        }
+        public static void mergesort(int arr[],int si,int ei){
+            if(si<=ei){
+                return;
+            }
+            int mid=si+(ei-si)/2;
+            mergesort(arr,si,ei);//iterator foe left part
+            mergesort(arr,mid+1,ei);
+
     }
     public static void main(String[] args) {
 
