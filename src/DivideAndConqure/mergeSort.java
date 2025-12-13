b44c63e0cf3394da09ce7528bc7f05f21a1fc3ed
pavanpai23 <pavanpai2006@gmail.com>
2025-12-13 22:56:56 +0530

    DSA

--- src/DivideAndConqure/mergeSort.java
+++ src/DivideAndConqure/mergeSort.java
@@ -12,10 +12,34 @@ public class mergeSort {
                 return;
             }
             int mid=si+(ei-si)/2;
-            mergesort(arr,si,ei);//iterator foe left part
-            mergesort(arr,mid+1,ei);
-
+            mergesort(arr,si,ei);//left part
+            mergesort(arr,mid+1,ei);// right part
+            merge(arr,si,mid,ei);
     }
+    public static void merge(int []arr,int si,int mid,int ei){
+        int temp[]=new int[si-ei+1];
+        int i=si;//iterator for  left part
+        int j=mid+1;//iterstion for right part
+        int k=0;//iterator for temp arr
+
+        while(i<=mid&&j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j>=mid){
+            temp[k++]=arr[j++];
+    }
+    }
+
     public static void main(String[] args) {
 
     }
