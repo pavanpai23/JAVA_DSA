b6d73f35c9bfdf012bbf1b4badb40ce8bec00602
pavanpai23 <pavanpai2006@gmail.com>
2025-12-14 23:11:43 +0530

    DSA

--- src/DivideAndConqure/mergeSort.java
+++ src/DivideAndConqure/mergeSort.java
@@ -3,26 +3,26 @@ package DivideAndConqure;
 public class mergeSort {
     public static void print(int[]arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + "");
+            System.out.print(arr[i] + " ");
         }
             System.out.println();
         }
         public static void mergesort(int arr[],int si,int ei){
-            if(si<=ei){
+            if(si>=ei){
                 return;
             }
             int mid=si+(ei-si)/2;
-            mergesort(arr,si,ei);//left part
+            mergesort(arr,si,mid);//left part
             mergesort(arr,mid+1,ei);// right part
             merge(arr,si,mid,ei);
     }
     public static void merge(int []arr,int si,int mid,int ei){
-        int temp[]=new int[si-ei+1];
+        int temp[]=new int[ei-si+1];
         int i=si;//iterator for  left part
         int j=mid+1;//iterstion for right part
         int k=0;//iterator for temp arr
 
-        while(i<=mid&&j<=ei){
+        while(i<=mid && j<=ei){
             if(arr[i]<arr[j]){
                 temp[k]=arr[i];
                 i++;
@@ -32,15 +32,22 @@ public class mergeSort {
             }
             k++;
         }
+        //LEFTPART
         while (i<=mid){
             temp[k++]=arr[i++];
         }
-        while(j>=mid){
+//        RIGHT PART
+        while(j<=ei){
             temp[k++]=arr[j++];
     }
+        for(k=0,i=si;k <temp.length ; k++,i++) {
+  arr[i]=temp[k];
+        }
     }
 
     public static void main(String[] args) {
-
+int [] arr={6,2,4,5,1,8};
+mergesort(arr,0, arr.length-1);
+print(arr);
     }
 }
