99566a20b77e334b5e7806b4255011bb1dbee1df
pavanpai23 <pavanpai2006@gmail.com>
2026-02-20 22:26:02 +0530

    DSA

--- src/DivideAndConqure/mergeSort.java
+++ src/DivideAndConqure/mergeSort.java
@@ -1,53 +1,53 @@
 package DivideAndConqure;
 
 public class mergeSort {
-    public static void print(int[]arr){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static void print(int[]nums){
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
         }
             System.out.println();
         }
-        public static void mergesort(int arr[],int si,int ei){
+        public static void mergesort(int nums[],int si,int ei){
             if(si>=ei){
                 return;
             }
             int mid=si+(ei-si)/2;
-            mergesort(arr,si,mid);//left part
-            mergesort(arr,mid+1,ei);// right part
-            merge(arr,si,mid,ei);
+            mergesort(nums,si,mid);//left part
+            mergesort(nums,mid+1,ei);// right part
+            merge(nums,si,mid,ei);
     }
-    public static void merge(int []arr,int si,int mid,int ei){
+    public static void merge(int []nums,int si,int mid,int ei){
         int temp[]=new int[ei-si+1];
         int i=si;//iterator for  left part
         int j=mid+1;//iterstion for right part
-        int k=0;//iterator for temp arr
+        int k=0;//iterator for temp nums
 
         while(i<=mid && j<=ei){
-            if(arr[i]<arr[j]){
-                temp[k]=arr[i];
+            if(nums[i]<nums[j]){
+                temp[k]=nums[i];
                 i++;
             }else{
-                temp[k]=arr[j];
+                temp[k]=nums[j];
                 j++;
             }
             k++;
         }
         //LEFTPART
         while (i<=mid){
-            temp[k++]=arr[i++];
+            temp[k++]=nums[i++];
         }
 //        RIGHT PART
         while(j<=ei){
-            temp[k++]=arr[j++];
+            temp[k++]=nums[j++];
     }
         for(k=0,i=si;k <temp.length ; k++,i++) {
-  arr[i]=temp[k];
+  nums[i]=temp[k];
         }
     }
 
     public static void main(String[] args) {
-int [] arr={6,2,4,5,1,8};
-mergesort(arr,0, arr.length-1);
-print(arr);
+int [] nums={6,2,4,5,1,8};
+mergesort(nums,0, nums.length-1);
+print(nums);
     }
 }
