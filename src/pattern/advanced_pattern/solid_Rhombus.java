ba4a3e7e23b85b3129b2c7d8e04af669e52837aa
pavanpai23 <pavanpai2006@gmail.com>
2026-08-18 22:47:16 +0530

    dsa

---
+++ src/pattern/advanced_pattern/solid_Rhombus.java
@@ -0,0 +1,17 @@
+package pattern.advanced_pattern;
+
+public class solid_Rhombus {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
